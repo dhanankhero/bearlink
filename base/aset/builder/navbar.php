74bc29b699b8ea6da988b24ae5fcaff616cbdcc6
dhanankhero <greatdhanank@gmail.com>
2019-02-17 15:18:39 +0700

    penambahan Clipboard.js dalam bearlink

--- base/aset/builder/navbar.php
+++ base/aset/builder/navbar.php
@@ -11,10 +11,21 @@
   </ul>
   <ul class="fkanan">
     <li class="boxData">https://bearlink.id/u/<?php echo $username ?></li>
-    <a href="#"><li class="copyData">Copy</li></a>
+    <a href="#"><li id="salin" class="copyData" data-clipboard-action="copy" data-clipboard-target=".boxData">Salin Link</li></a>
     <a href="#" id="akun"><li class="copyData" id="nama-akun"><?php echo $name ?></li></a>
     <div class="clr"></div>
   </ul>
   <div class="clr"></div>
 </div>
+<script src="../base/aset/clipboard/dist/clipboard.min.js"></script>
+<script>
+  var copyToIn = new ClipboardJS('#salin');
+
+  copyToIn.on('success', function(e) {
+    console.log(e);
+  });
+  copyToIn.on('error', function(e) {
+    console.log(e);
+  });
+</script>
 <?php include 'optionAccount.php' ?>
